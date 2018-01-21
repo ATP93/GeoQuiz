05858d7b4202f4f11dc040f16b06a56089ecf1d2
Denis Galajda <galajda.denis@gmail.com>
2018-01-21 13:54:31 +0100

    create Question class

---
+++ app/src/main/java/com/denisgalajda/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.denisgalajda.geoquiz;
+
+/**
+ * Created by Denis Galajda on 21.1.18.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
