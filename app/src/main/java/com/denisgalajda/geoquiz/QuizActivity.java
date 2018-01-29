f81eac905ec9c3673d5c5a21d2eff29ead9e45cc
Denis Galajda <galajda.denis@gmail.com>
2018-01-29 23:01:33 +0100

    show question answer in CheatActivity

--- app/src/main/java/com/denisgalajda/geoquiz/QuizActivity.java
+++ app/src/main/java/com/denisgalajda/geoquiz/QuizActivity.java
@@ -64,7 +64,8 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //start CheatActivity
-                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
                 startActivity(intent);
             }
         });
